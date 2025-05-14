db88a5fb8edf6f2a63bf6a6284414ee46aa49b5f
carolvfq <carolvfdq@gmail.com>
2025-05-14 09:29:46 -0300

    implementaçao de funçoes

--- banco.c
+++ banco.c
@@ -31,8 +31,8 @@ void efetuarSaque();
 void efetuarDeposito();
 void efetuarTansferencia();
 void listarContas();
-void atualizaSaldoTotal(Conta conta);
-void buscarContaPorNumero(int numero);
+float atualizaSaldoTotal(Conta conta);
+Conta buscarContaPorNumero(int numero);
 void sacar(Conta conta, float valor);
 void depositar(Conta conta, float valor);
 void transferir(Conta conta_origem, Conta conta_destino, float valor);
@@ -48,16 +48,20 @@ int main(){
 }
 
 void menu(){
+    printf("---------------------------------");
+    printf("--------------BANCO--------------");
+    printf("---------------------------------");
     printf("escolha uma das opçoes:");
-    printf("1-Criar conta:\n");
-    printf("2-Efetuar saque:\n");
-    printf("3-Efetuar deposito:\n");
-    printf("4-Efetuar transferencia:\n");
-    printf("5-Listar contas:\n");
+    printf("1-Criar conta\n");
+    printf("2-Efetuar saque\n");
+    printf("3-Efetuar deposito\n");
+    printf("4-Efetuar transferencia\n");
+    printf("5-Listar contas\n");
+    printf("6-Sair do sistema\n");
     
     int opcoes;
     scanf("%d",&opcoes);
-    
+    getchar();
     switch (opcoes)
     {
     
@@ -76,18 +80,25 @@ void menu(){
     case 5:
         listarContas();
         break;    
-    
+    case 6:
+        listarContas();
+        Sleep(2000);
+        exit(0);
+        menu();
+        break;  
         
     default:
     printf("opcao invalida.\n");
+        Sleep(2000);
         break;
     }
 
 }
 void infoCliente(Cliente cliente){
-
+    printf("Nome: %s\n Codigo:%d\n CPF:  %s\n email:%s\n Data de nascimento %s\n Data do cadastro: %s\n", cliente.nome, cliente.codigo, cliente.cpf, cliente.email,cliente.dataNascimento,cliente.dataCadastro );
 }
 void infoConta(Conta conta){
+    printf("Nome:%s\n CPF:%s\nNumero:%d\n Saldo:%d\n Limite:%d\n  Saldo Total:%d\n",conta.cliente.nome,conta.cliente.cpf,conta.numero,conta.saldo,conta.saldoTotal);
 
 }
 void criarConta(){
@@ -105,11 +116,19 @@ void efetuarTansferencia(){
 void listarContas(){
 
 }
-void atualizaSaldoTotal(Conta conta){
+float atualizaSaldoTotal(Conta conta){
+    return conta.saldo + conta.limite;
 
 }
-void buscarContaPorNumero(int numero){
-
+Conta buscarContaPorNumero(int numero){
+     Conta C;
+    if(contador_contas>0){
+        for(int i=0 ; i < contador_contas;i++){
+             if(contas[i].numero == numero){
+                 C=contas[i];
+             }
+        }
+    }
 }
 void sacar(Conta conta, float valor){
 
